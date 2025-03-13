c9bea8e1383e29ea2f7764dc63b8bdd477e40080
Aeolin <prisaeolin@gmail.com>
2025-03-13 16:53:19 +0700

    add tasks from login to checkout

---
+++ locator.py
@@ -0,0 +1,21 @@
+username_field = '//android.widget.EditText[@content-desc="test-Username"]'
+password_field = '//android.widget.EditText[@content-desc="test-Password"]'
+login_button = '//android.view.ViewGroup[@content-desc="test-LOGIN"]'
+
+filter_button = '//android.view.ViewGroup[@content-desc="test-Modal Selector Button"]'
+price_high_to_low = '//android.widget.TextView[@text="Price (high to low)"]'
+
+sauce_labs_fleece = '//android.view.ViewGroup[@content-desc="test-ADD TO CART"][1]'
+# sauce_labs_backpack = '//android.widget.TextView[@text="ADD TO CART"][2]'
+
+cart = '//android.view.ViewGroup[@content-desc="test-Cart"]'
+
+checkout_button = '//android.view.ViewGroup[@content-desc="test-CHECKOUT"]'
+
+first_name_field = '//android.widget.EditText[@content-desc="test-First Name"]'
+last_name_field = '//android.widget.EditText[@content-desc="test-Last Name"]'
+zip_code_field = '//android.widget.EditText[@content-desc="test-Zip/Postal Code"]'
+continue_button = '//android.view.ViewGroup[@content-desc="test-CONTINUE"]'
+
+checkout_overview = '//android.widget.TextView[@text="CHECKOUT: OVERVIEW"]'
+finish_button = '//android.view.ViewGroup[@content-desc="test-FINISH"]'
